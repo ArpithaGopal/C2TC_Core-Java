ba280276883120bf3c6c4508a04a26f9f153c101
Admin <Admin@DESKTOP-03LMG27>
2022-06-03 13:30:56 +0530

    first commit

---
+++ Demo 04/src/TestThis5.java
@@ -0,0 +1,19 @@
+class Demo{
+	Demo(){
+		this(10);
+		System.out.println("hello a");
+	}
+	Demo(int x){
+		//this();
+		System.out.println(x);
+	}
+}
+public class TestThis5 {
+	
+		public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Demo a= new Demo();
+
+	}
+
+}
