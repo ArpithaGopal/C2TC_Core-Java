33f4c029af26ed1b4ba75715bc8f694af1a74baf
Admin <Admin@DESKTOP-03LMG27>
2022-06-07 14:48:16 +0530

    second commit

---
+++ Buffers/src/Main2.java
@@ -0,0 +1,15 @@
+
+public class Main2 {
+
+	public static void main(String[] args) {
+		{
+			StringBuffer sb=new StringBuffer("Hello");
+			sb.append("Java");
+			System.out.println(sb);
+			
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
